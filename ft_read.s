e4946656c422f980fcfe0d836ac1bb2bd51b8b49
Redwane ZAFARI <rzafari@e3r3p21.42.fr>
2020-04-11 23:15:38 +0200

    added ft_read

---
+++ ft_read.s
@@ -0,0 +1,11 @@
+global _ft_read
+
+_ft_read:
+	mov rax,0x2000003 ;put the number corresponding to sys_read in rax
+	syscall			  ;ask the OS to call the correct function
+	jc exit			  ;if it returns a negative value => exit
+	ret				  ;else: return the value returned by 'read'
+
+exit:
+	mov rax,-1		  ;if 'read' returns a negative value, we put '-1' in rax
+	ret				  ; we return rax
