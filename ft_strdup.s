fb267679d31a68f43193c01002a2a707b37c03e6
rzafari42 <rzafari@student.42.fr>
2020-05-11 19:07:26 +0200

    OK

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,24 +1,21 @@
-section	.text
-global	ft_strdup
+section .text
 extern	ft_strlen
 extern	ft_strcpy
 extern	malloc
+global	ft_strdup
 
 ft_strdup:
-	push	rbp
-	;call	ft_strlen
-	inc 	rax
-	mov	r8,rdi
-	mov	rdi,rax
+	call	ft_strlen	;rax = ft_strlen(rdi);
+	inc	rax		; rax = rax + 1 (append the '\0')
+	push	rdi		;save rdi before we change it
+	mov	rdi,rax		;rdi = rax (len), because malloc need it to know which size to allocate
 	call	malloc
-	cmp	rax,0
-	jle	malloc_error
-	mov	rsi,r8
-	mov	rdi,rax
-	;call	ft_strcpy
-	pop	rbp
+	cmp	rax,0		;if malloc returned a negative value
+	jz	malloc_error	;exit
+	pop	rsi		;rsi takes the value wue pushed at the begining, so rsi = (old)rdi = src
+	mov	rdi,rax		;rdi takes the address to the allocated memory
+	call	ft_strcpy	;call ft_strcpy(const char *dst, const char *src) w/ dst = rdi & src = rsi
 	ret
-
-malloc_error:	
-	XOR rax,rax
+	
+malloc_error:
 	ret
