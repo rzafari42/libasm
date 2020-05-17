8829b71abd0e6c19d0f629ce2bfb5c396b2099d6
rzafari42 <redwane.zafari@hotmail.com>
2020-05-17 17:53:35 +0200

    ok

--- libasm.h
+++ libasm.h
@@ -8,10 +8,16 @@
 # include <string.h>
 # include <errno.h>
 
-size_t ft_strlen(const char *s);
+typedef	struct		s_list
+{
+	void	*data;
+	struct	s_list	*next;
+}			t_list;
+
+size_t	ft_strlen(const char *s);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
 ssize_t ft_read(int fd, void *buf, size_t count);
-char 	*ft_strcpy(char *dst,const char *src);
+char	*ft_strcpy(char *dst,const char *src);
 char	*ft_strdup(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 void	ft_len(void);
